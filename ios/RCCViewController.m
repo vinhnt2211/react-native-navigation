2249287f2db10ea2dad47ab54498b36c181b3440
Vinh <vinh.nguyen@enouvo.com>
2018-01-14 01:36:40 +0700

    fix crash when set tabbar hide.

--- ios/RCCViewController.m
+++ ios/RCCViewController.m
@@ -787,7 +787,7 @@ const NSInteger TRANSPARENT_NAVBAR_TAG = 78264803;
 - (BOOL)hidesBottomBarWhenPushed
 {
   if (!self._hidesBottomBarWhenPushed) return NO;
-  return (self.navigationController.topViewController == self) && ![(RCCTabBarController*)self.tabBarController tabBarHidden];
+  return (self.navigationController.topViewController == self);
 }
 
 - (BOOL)prefersStatusBarHidden
